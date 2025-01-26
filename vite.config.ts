803c596768e51c46889a4bf6d5511a9b20741f09
Namy3242 <something-3242@hotmail.co.jp>
2025-01-26 23:27:59 +0900

    actionsを追加

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/my-app/', // リポジトリ名に置き換えてください
   plugins: [react()],
+  define: {
+    'process.env': process.env
+  }
 })
